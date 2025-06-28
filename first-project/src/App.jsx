10db44ad6d080d49646430e97d9d9accf1dcaabb
Bhabin Dulal <bhabindulal35@gmail.com>
2025-06-28 12:00:00 +0545

    Functional Component

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -4,17 +4,37 @@ import './App.css'
 
 const name = "Bhabin"
 const element = <h2>I am learning React..</h2>
+const Search = ()=> <input placeholder =" Sear your fav product"/> 
+const Button = ()=> <button> Submit</button>
+
+const Header = ()=>{
+  return (
+    <><div style={{backgroundColor:'blueviolet'}}>
+    <h1> Musify</h1>
+    <input placeholder = 'Search for music'/>
+    <ul style={ { display:'flex', gap:'100px'}}>
+      <li> Home</li>
+      <li> About</li>
+      <li> Contact us</li>
+    </ul>
+    </div>
+    </>
+  )
+}
 function App() {
   return (
     <>
+    
     <div>
       <h1>Hello i am {name}</h1>
       {element}
-      
+      <Button/>
+      <div>
+      <Search/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+      </div>
+     
     </>
   )
 }
