eac0250c3275dabc5276ac2caf205afa2d27a393
Bhabin Dulal <bhabindulal35@gmail.com>
2025-06-29 21:43:21 +0545

    ex-2

--- Exercise-1/src/App.jsx
+++ Exercise-1/src/App.jsx
@@ -1,38 +1,55 @@
 import { useState } from 'react';
 import './App.css'
+import {v4 as uuid} from 'uuid';
 
-const allBrands = [
-  {id:"1", brandName :"puma"},
-  {id:"2", brandName :"fila"},
-  {id:"3", brandName :"reebok"},
-  {id:"4", brandName :"nike"},
-  {id:"5", brandName :"addidas"},
 
-]
-  
 
 
 function App() {
 
-  const [search, setsearch] = useState()
-  const [brands, setBrands] = useState(allBrands)
+  const [todo, setTodo] = useState()
+  const [todoList, setTodoList] = useState([])
 
-  const onSearchChange = (e) => {
-  setsearch(e.target.value);
-  const filteredBrands = e.target.value?.length>0 ? brands.filter(brand => brand.brandName.includes(e.target.value.toLowerCase())): allBrands;
-  setBrands(filteredBrands)
-}
+  const onTodoInputChange =(e) => {
+    setTodo(e.target.value)
+  }
+  const onAddToDoClick = ()=>{
+    setTodoList([...todoList, {id: uuid(), todo: todo , isCompleted : false}])
+    setTodo('')
+  }
+  const onDeleteClick =(id)=>{
+    const updatedToDoList = todoList.filter(todo => todo.id !==id)
+    setTodoList(updatedToDoList)
+  }
+  const onTodoCheckChange = (id )=>{
+    setTodoList = todoList.map(todo =>todo.id === id? {...todo, isCompleted: !todo.isCompleted}:todo)
+    setTodoList(updatedToDoList)
+  }
+
 
   return (
     <>
-    <input onChange ={onSearchChange} placeholder='Search a brand'/>
-    <ul>
+    <div lassName = "app">
+      <h1> My WishList</h1>
+    </div>
+    <div>
+      <input value = {todo} onChange={onTodoInputChange} placeholder='Add your Wishlist' />
+      <button onClick={onAddToDoClick}> Add </button>
+    </div>
+    <div>
       {
-        brands.map(brand => <li>{brand.brandName}</li>)
+        todoList?.length > 0 && todoList.map( todo =>(
+          <div key ={todo.id}>
+            <label>
+              <input onChange={() => onTodoCheckChange(todo.id)} type="checkbox" />
+              <span className= {todo.isCompleted ? 'strike-through' : ''}> {todo.todo}</span>
+            </label>
+            <button onClick={()=> onDeleteClick(todo.id)}> Delete </button>
+          </div>)
+        )
       }
-    </ul>
+    </div>
 
-      
     </>
   )
 }
